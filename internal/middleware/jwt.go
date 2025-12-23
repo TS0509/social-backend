bfb97ca4a988e485ab7ec09b5efe7eb0d92b9451
TS0509 <thanchee5354@gmail.com>
2025-12-23 18:19:53 +0800

    new upload

---
+++ internal/middleware/jwt.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func GenerateToken(userID uint) (string, error) {
+	secret := os.Getenv("JWT_SECRET")
+
+	claims := jwt.MapClaims{
+		"userID": userID,
+		"exp":    time.Now().Add(24 * time.Hour).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(secret))
+}
